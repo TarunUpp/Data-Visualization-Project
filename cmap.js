c21cc45155b96834b2cf66b365c4ade4ba547891
TarunUpp <92453558+TarunUpp@users.noreply.github.com>
2021-11-09 17:47:53 -0500

    Update cmap.js

--- cmap.js
+++ cmap.js
@@ -1,6 +1,6 @@
 
 d3.json("output.geojson").then(function(themap){
-    d3.csv("confirmed_cases.csv").then(function(dataset){
+    d3.csv("PEdited_confirmed_cases.csv").then(function(dataset){
     
       
         console.log(themap)
@@ -8,17 +8,19 @@ d3.json("output.geojson").then(function(themap){
        
         var svg=d3.select("svg")
         .style("width",2000)
-        .style("height",500)
+        .style("height",400)
        //colorScale = d3.scaleThreshold() 
          //           .domain([0,0.1*d3.max(Object.values(popPerCountry)),d3.max(Object.values(popPerCountry))])
            //        .domain([5000, 10000, 20000, 50000, 100000, 250000, 500000, 750000, 1000000, 1500000])
             //      .range(["#4e79a7" , "#f28e2c", "#e15759", "#76b7b2", "#59a14f", "#edc949", "#af7aa1", "#ff9da7", "#9c755f", "#bab0ab"])
         var projection=d3.geoEqualEarth() //geoMercator geoEqualEarth
                         .fitWidth(800,{type:"Sphere"})
-        var pathGenerator=d3.geoPath(projection)
+        var pathGenerator=d3.geoPath(projection)       
         var sea=svg.append("path")
                     .attr("d",pathGenerator({type:"Sphere"}))
                     .attr("fill","lightblue")
+
+        var headers = ["0", "1000", "100000", "500000", "2500000", "30000000"];
         // var legendText = ['0', '1000', '100000', '500000', '2500000', '30000000'];
         // var legendColors = d3.range(d3.schemeReds[8])
         
@@ -82,6 +84,22 @@ d3.json("output.geojson").then(function(themap){
                             .style("opacity", .8)
                             .on("mouseover", mouseOver )
                             .on("mouseleave", mouseLeave )
- 
+        var legend = svg.selectAll(".legend")
+                        .data(headers.slice().reverse())
+                        .enter().append("g")
+                        .attr("class", "legend")
+                        .attr("transform", function(d, i) { return "translate(-20," + i * 20 + ")"; });
+                           
+            legend.append("rect")
+                    .attr("x", 870)
+                    .attr("width", 20)
+                    .attr("height", 15)
+                    .style("fill", colorScale);
+                        
+            legend.append("text")
+                    .attr("x", 850)
+                    .attr("y", 15)
+                    .style("text-anchor", "end")
+                    .text(function(d) { return d;  });
         })
     })
